07b640d8d1f9f7eac296c7051730ec22d77257d8
Yahan Botheju <yahan.ravinga.git@gmail.com>
2026-03-10 22:11:40 +0530

    feature: implement complete 2fa functionality

--- backend/routers/user.route.js
+++ backend/routers/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { userRegister, userLogin, refreshAccessToken, userLogout } from '../controllers/user.controller.js';
+import { userRegister, userLogin, refreshAccessToken, userLogout, setup2FA, verify2fa, login2FAConfirm } from '../controllers/user.controller.js';
 import { protect } from '../middlewares/auth.middleware.js'
 
 const router = express.Router();
@@ -12,6 +12,16 @@ router.post('/login', userLogin);
 
 //Refresh Token route
 router.post('/refresh', refreshAccessToken);
+
+/* 2FA routes */
+//set up route
+router.post('/2fa/setup', protect, setup2FA)
+
+//verify route
+router.post('/2fa/verify', protect,  verify2fa)
+
+//confrim 2FA
+router.post('/login/2fa-confirm', login2FAConfirm)
 
 //Logout User
 router.post('/logout', userLogout);
